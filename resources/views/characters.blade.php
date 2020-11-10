326c41c37496a8ddd16baf93f38760abd02e604e
Jeannine Prueger <jeannine.prueger@gmail.com>
2020-11-10 15:27:34 +0100

    added foreach through data to get the details of each character

--- resources/views/characters.blade.php
+++ resources/views/characters.blade.php
@@ -2,6 +2,20 @@
 
 @section('container')
 
-    <p>test</p>
+    @foreach($data['results'] as $person)
+
+        {{-- {{printf($person['name'])}} --}}
+
+        <h2>{{ $person['name'] }}</h2>
+        <p>Height: {{ $person['height'] }}</p>
+        <p>Mass: {{ $person['mass'] }}</p>
+        <p>Hair Color: {{ $person['hair_color'] }}</p>
+        <p>Skin Color: {{ $person['skin_color'] }}</p>
+        <p>Eye Color: {{ $person['eye_color'] }}</p>
+        <p>Birth Year: {{ $person['birth_year'] }}</p>
+        <p>Gender: {{ $person['gender'] }}</p>
+
+
+    @endforeach
 
 @endsection
