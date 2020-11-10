08c4f1069647982568425b33c4f677c93a5d8cb3
Jeannine Prueger <jeannine.prueger@gmail.com>
2020-11-10 14:09:33 +0100

    created public function to show characters (index)

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;  
 
 class CharacterController extends Controller
 {
-    //
+    public function index () {
+        // mittels http get request von der api die people holen -> in ein json()
+        $data = Http::get('https://swapi.dev/api/people/')->json();
+
+        // an die view zurückgeben (data ist das komplette array von people)
+        return view('characters', ['data' => $data]);
+    }
 }
